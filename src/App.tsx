f729c083617dfb4a807dbf746cf700ce48583ba6
续国靖 <xuguojing77@163.com>
2023-08-10 17:59:33 +0800

    #字棋状态提升

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,50 @@
 // react 组件是返回标签的 JavaScript 函数
 // react 组件必须以大写开头 而HTML标签则必须以小写开头
 import './public.css';
-import React from "react";
 import {useState} from "react";
 // React 程序是由"组件"组成的。一个组件是UI(用户界面)的一部分, 它拥有自己的逻辑和外观。
 // 组件可以小到一个按钮,也可以大到整个页面
-interface MySquare {
-    digit: number | string
-}
-const Square: React.FC<MySquare> = ({digit}) => {
-    const [value, setValue] = useState(null)
+
+const Square = () => {
+    // 初始化状态
+    const [value, setValue] = useState("")
+
+    // 点击棋盘上的按钮设置x
     function handleClick() {
-        console.log('clicked!')
+        setValue("x")
     }
+
     return (
         <>
-            <button className="square" onClick={handleClick}>{digit}</button>
+            <button className="square" onClick={handleClick}> {value} </button>
         </>
     )
 }
 
 function App() {
+    // 状态提升
+    // 在父组件中声明共享state, 父组件可以通过props将state传回子组件, 使数据同步
+    // Array(9).fill("") 创建了一个包含9个元素的数组, 并将每个元素设置为"",
+    // ["","","x","x","x","o","o","o",""]
+    const [squares, setSquares] = useState(Array(9).fill(""))
+
     return (
+        // 构建棋盘
         <>
             <div className={"board-row"}>
-                <Square digit={1}/>
-                <Square digit={2}/>
-                <Square digit={3}/>
+                <Square value={squares[0]}/>
+                <Square value={squares[1]}/>
+                <Square value={squares[2]}/>
             </div>
             <div className={"board-row"}>
-                <Square digit={4}/>
-                <Square digit={5}/>
-                <Square digit={6}/>
+                <Square value={squares[3]}/>
+                <Square value={squares[4]}/>
+                <Square value={squares[5]}/>
             </div>
             <div className={"board-row"}>
-                <Square digit={7}/>
-                <Square digit={8}/>
-                <Square digit={9}/>
+                <Square value={squares[6]}/>
+                <Square value={squares[7]}/>
+                <Square value={squares[8]}/>
             </div>
         </>
     )
