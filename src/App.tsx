74b9c9397ed467c934ace5ac37a25631ea0fece8
续国靖 <xuguojing77@163.com>
2023-09-11 11:53:10 +0800

    interface类型声明

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // 使用typescript编写react
 
 // 内联语法
-const Mybutton = ({title, name}: { title: string, name: string }) => {
+const MyButton = ({title, name}: { title: string, name: string }) => {
     return (
         <>
             <h4>{name}</h4>
@@ -11,10 +11,28 @@ const Mybutton = ({title, name}: { title: string, name: string }) => {
 }
 
 
+// interface type 类型
+interface MyButtonProps {
+    //按钮文字
+    title: string;
+    //按钮是否禁用
+    disabled: boolean;
+}
+
+const MyBottons = ({title, disabled}: MyButtonProps) => {
+    return (
+        <>
+            <button disabled={disabled}> {title} </button>
+        </>
+    )
+}
+
 export default function App() {
     return (
         <>
-            <Mybutton title={'我是一个按钮'} name={'小明'}></Mybutton>
+            <MyButton title={'我是一个按钮'} name={'小明'}></MyButton>
+            <br/>
+            <MyBottons title={'我是禁用按钮'} disabled={true}></MyBottons>
         </>
     )
 }
