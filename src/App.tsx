f7e1c7b2d841a97d7b5932d7985a9c69b233fe21
续国靖 <xuguojing77@163.com>
2023-11-04 13:52:33 +0800

    -

--- src/App.tsx
+++ src/App.tsx
@@ -218,11 +218,13 @@ function App() {
             {/*步骤一：将Props传递给子组件*/}
             {/*例如： person(一个对象) 和 size (一个数字)*/}
             {/*person=后面的双花括号 在JSX模板中它们只是一个对象*/}
-            <Avatar   person={{
+            <Avatar
+                size={100}
+                person={{
                 name: 'Katsuko Saruhashi',
                 imageId: 'YfeOqp2'
-            }}
-                      size={100} ></Avatar>
+                }}
+             / >
             <Avatar
                 size={80}
                 person={{
