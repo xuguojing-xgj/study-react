d01910ad1cd67d69519bf5f2c8fb2f5f19e5c92b
续国靖 <xuguojing77@163.com>
2023-11-21 11:16:04 +0800

    update files

--- src/App.tsx
+++ src/App.tsx
@@ -12,20 +12,44 @@ import './css/01-index.css';
 // import _a7 from './assets/7.jpg';
 // import _a8 from './assets/8.jpg';
 import _a9 from './assets/9.jpg';
+
+
+const BottomModule = () => {
+    return (
+        <>
+            <div style={{position:"fixed", bottom:0, width:'100%', height:'50px', textAlign:'center',lineHeight:'50px'}}>
+                <a style={{color:'#000'}} href="https://beian.miit.gov.cn/" target="_blank">备案号:京ICP备2023032161号-1</a>
+            </div>
+        </>
+    )
+}
+
 function App() {
 
     return (
         <>
             <div className={'landscape'}>
-              <div className={'son'}>
-                  {/*宋代苏轼的《点绛唇·闲倚胡床》 */}
-                  <div className={'sizeStyle'}>闲倚胡床，庾公楼外峰千朵。与谁同坐。明月清风我。</div>
-                  {/*<img className={'_a3img'} src={_a3} alt={''}/>*/}
-                  {/*<img className={'_a3img'} src={_a7} alt={''}/>*/}
-                  {/*<img className={'_a3img'} src={_a8} alt={''}/>*/}
-                  <img className={'_a3img'} src={_a9} alt={''}/>
-              </div>
+                <div className={'son'}>
+                    {/*宋代苏轼的《点绛唇·闲倚胡床》 */}
+                    <div className={'sizeStyle'}>
+                        闲倚胡床，庾公楼外峰千朵。与谁同坐。明月清风我。
+                        <br/>
+                        <div style={{
+                            width: '100%',
+                            textAlign: 'end',
+                            fontSize: '16px',
+                            margin: '10px 0'
+                        }}>-宋代苏轼的《点绛唇·闲倚胡床》
+                        </div>
+                    </div>
+                    {/*<img className={'_a3img'} src={_a3} alt={''}/>*/}
+                    {/*<img className={'_a3img'} src={_a7} alt={''}/>*/}
+                    {/*<img className={'_a3img'} src={_a8} alt={''}/>*/}
+                    <img className={'_a3img'} src={_a9} alt={''}/>
+                </div>
             </div>
+
+            <BottomModule></BottomModule>
         </>
     )
 }
