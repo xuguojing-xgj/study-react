f0f7bc6544f6326165832a136d17dfe2ae3fc026
xuguojing-xgj <xuguojing77@163.com>
2024-04-10 15:20:32 +0800

    chore: useReducer 基本使用

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@
 // 第三步添加标签 function Profile() { return ( <> <h1> hello world </h1> </> ) }
 // 组件返回 JSX标签
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useLayoutEffect, useState, useReducer } from 'react';
+import type { Reducer } from 'react';
 import './css/01-index.css';
 
 async function queryData() {
@@ -20,14 +21,38 @@ async function queryData() {
 	return data;
 }
 
+interface Data {
+	result: number;
+}
+
+interface Action {
+	type: 'add' | 'minus';
+	num: number;
+}
+
+function reducer(state: Data, action: Action) {
+	switch (action.type) {
+		case 'add':
+			return {
+				result: state.result + action.num
+			};
+		case 'minus':
+			return {
+				result: state.result - action.num
+			};
+	}
+
+	return state;
+}
+
 function App() {
 	const [num, setNum] = useState(0);
 
-    // 当依赖数组变化时就会执行 useEffect
-    // 类似于 useLayouEffect
-    // useEffect 执行时间长避免页面卡顿掉帧
-    // useLayoutEffect 页面闪动问题比较严重可以使用
-	useEffect(() => {
+	// 当依赖数组变化时就会执行 useEffect
+	// 类似于 useLayouEffect
+	// useEffect 执行时间长避免页面卡顿掉帧
+	// useLayoutEffect 页面闪动问题比较严重可以使用
+	useLayoutEffect(() => {
 		console.log('effect');
 		const timer = setInterval(() => {
 			console.log(num);
@@ -40,9 +65,25 @@ function App() {
 			clearInterval(timer);
 		};
 	}, [num]); // 依赖数组
+
+	// 修改值 之前做一些固定的逻辑 useReducer
+	// const [state, dispatch] = useReducer(reducer, initialArg, init?)
+	const [state, dispatch] = useReducer<Reducer<Data, Action>>(reducer, {
+		result: 0
+	});
+
 	return (
 		<>
 			<div onClick={() => setNum((prevNum) => prevNum + 1)}> {num} </div>
+
+			<button type="button" onClick={() => dispatch({ type: 'add', num: 1 })}>
+				加
+			</button>
+			<button type="button" onClick={() => dispatch({ type: 'minus', num: 1 })}>
+				减
+			</button>
+
+			<div> {state.result} </div>
 		</>
 	);
 }
