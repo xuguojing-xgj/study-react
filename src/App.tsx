dfb478c5328dde7ea405605fb28a333e0ad71fd6
xuguojing-xgj <xuguojing77@163.com>
2024-07-05 17:28:47 +0800

    docs: code update

--- src/App.tsx
+++ src/App.tsx
@@ -2,72 +2,83 @@ import React, { useState, ChangeEvent } from 'react';
 import { Button, Input, Checkbox } from 'antd';
 import type { CheckboxProps } from 'antd';
 const App = () => {
-	function createTodos() {
-		console.log('1');
-	}
-	const [todos, setTodos] = useState(() => createTodos());
+    function createTodos() {
+        console.log('1');
+    }
+    const [todos, setTodos] = useState(() => createTodos());
 
-	// 为组件添加状态
-	const [age, setAge] = useState(42);
-	const [name, setName] = useState('Taylor');
-	const [text, setText] = useState('hello');
-	const [check, setCheck] = useState(false);
+    // 为组件添加状态
+    const [age, setAge] = useState(42);
+    const [name, setName] = useState('Taylor');
+    const [text, setText] = useState('hello');
+    const [check, setCheck] = useState(false);
 
-	const [newName, setNewName] = useState('小明');
-	const [newAge, setNewAge] = useState(18);
-	function handleClick() {
-		setName('Robin');
-		setAge((a) => a + 1);
-		setText('hello');
-	}
+    const [newName, setNewName] = useState('小明');
+    const [newAge, setNewAge] = useState(18);
+    function handleClick() {
+        setName('Robin');
+        setAge((a) => a + 2);
+        setText('hello');
+    }
 
-	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-		setText(e?.target?.value);
-	};
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        console.log('当前事件对象', e);
+        setText(e?.target?.value);
+    };
 
-	const checkChange: CheckboxProps['onChange'] = () => {
-		setCheck(() => !check);
-	};
-	return (
-		<>
-			<div>
-				{name}
-				<div>{age}</div>
+    const checkChange: CheckboxProps['onChange'] = () => {
+        setCheck(() => !check);
+    };
+    return (
+        <>
+            <div>
+                {name}
+                <div>{age}</div>
 
-				<div>
-					<Input
-						value={text}
-						onChange={handleChange}
-						placeholder="Basic usage"
-					/>
-					{text}
-				</div>
+                <div>
+                    <Input
+                        value={text}
+                        onChange={handleChange}
+                        placeholder="Basic usage"
+                    />
+                    {text}
+                </div>
 
-				<div>
-					<Checkbox checked={check} onChange={checkChange}>
-						{check ? 'check' : 'no check'}
-					</Checkbox>
-				</div>
-				<Button onClick={() => handleClick()} type="primary">
-					Primary Button
-				</Button>
+                <div>
+                    <Checkbox checked={check} onChange={checkChange}>
+                        {check ? 'check' : 'no check'}
+                    </Checkbox>
+                </div>
+                <Button onClick={() => handleClick()} type="primary">
+                    Primary Button
+                </Button>
 
-				<div>
-					<Input
-						value={newName}
-						onChange={(e: ChangeEvent<HTMLInputElement>) =>
-							setNewName(e.target.value)
-						}
-						placeholder="Basic usage"
-					/>
-					<Button type="primary" onClick={() => setNewAge(newAge + 1)}>
-						按钮
-					</Button>
-					Hello,{newName}, you are {newAge}
-				</div>
-			</div>
-		</>
-	);
+                <div>
+                    <Input
+                        value={newName}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                            setNewName(e.target.value)
+                        }
+                        placeholder="Basic usage"
+                    />
+                    <Button
+                        type="primary"
+                        onClick={() => setNewAge(newAge + 1)}
+                    >
+                        按钮
+                    </Button>
+                    Hello,{newName}, you are {newAge}
+                </div>
+            </div>
+
+            <div>
+                
+                {/* 表单对象 */}
+
+
+            </div>
+        </>
+    );
 };
 
 export default App;
