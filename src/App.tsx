b84d917525fd5a21e179a36985633282021b86f5
xuguojing-xgj <xuguojing77@163.com>
2024-10-17 00:11:54 +0800

    docs: calendar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, {
+    useState,
+    useEffect,
+    ForwardRefRenderFunction,
+    useRef,
+    useImperativeHandle,
+} from 'react';
 import './css/02-index.css';
-
+import { useControllableValue } from 'ahooks';
 interface CalendarProps {
+    value?: Date;
     defaultValue?: Date;
     onChange?: (date: Date) => void;
 }
 
-const Calendar = (props: CalendarProps) => {
-    const { defaultValue, onChange } = props;
-    const [date, setDate] = useState(defaultValue);
+interface CalendarRef {
+    getDate: () => void;
+    setDate: (date: Date) => void;
+}
 
+const InternalCalendar: ForwardRefRenderFunction<CalendarProps, CalendarRef> = (
+    props: CalendarProps,
+    ref: CalendarRef
+) => {
+    const { value, defaultValue, onChange } = props;
+    console.log(props);
+    const [date, setDate] = useControllableValue(props, {
+        defaultValue: new Date(),
+    });
+    useImperativeHandle(ref, () => {
+        return {
+            getDate() {
+                return date;
+            },
+            setDate(date: Date) {
+                setDate(date);
+            },
+        };
+    });
     const handleprevMonth = () => {
         setDate(new Date(date.getFullYear(), date.getMonth() - 1, 1));
     };
@@ -52,9 +79,22 @@ const Calendar = (props: CalendarProps) => {
         }
 
         for (let i = 1; i <= daysCount; i++) {
+            const clickHandler = () => {
+                const currentDate = new Date(
+                    date.getFullYear(),
+                    date.getMonth(),
+                    i
+                );
+                setDate(currentDate);
+                // onChange?.(currentDate);
+            };
             const isToday = i === date.getDate();
             days.push(
-                <div key={i} className={`day ${isToday ? 'selected' : ''}`}>
+                <div
+                    key={i}
+                    className={`day ${isToday ? 'selected' : ''}`}
+                    onClick={() => clickHandler()}
+                >
                     {i}
                 </div>
             );
@@ -86,12 +126,36 @@ const Calendar = (props: CalendarProps) => {
     );
 };
 
+const Calendar = React.forwardRef(InternalCalendar);
+
 const App = () => {
+    const calendarRef = useRef<CalendarRef>(null);
+
+    useEffect(() => {
+        console.log(calendarRef.current?.getDate().toLocaleDateString());
+
+        setTimeout(() => {
+            calendarRef.current?.setDate(new Date(2044, 10, 0));
+        }, 3000);
+    }, []);
     return (
         <div>
-            {/* <Calendar></Calendar> */}
-            <Calendar defaultValue={new Date('2023-3-1')}></Calendar>
-            <Calendar defaultValue={new Date('2023-8-1')}></Calendar>
+            {/* <Calendar
+                ref={calendarRef}
+                defaultValue={new Date('2024-10-16')}
+            ></Calendar> */}
+            <Calendar
+                value={new Date('2023-3-1')}
+                onChange={(date: Date) => {
+                    alert(date.toLocaleDateString());
+                }}
+            ></Calendar>
+            <Calendar
+                defaultValue={new Date('2023-8-1')}
+                onChange={(date: Date) => {
+                    alert(date.toLocaleDateString());
+                }}
+            ></Calendar>
         </div>
     );
 };
